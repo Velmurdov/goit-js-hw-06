65e2d98445387c708559e42c531894d02f35dd41
Batyr Velmuradov <120122361+Velmurdov@users.noreply.github.com>
2023-06-10 16:43:58 +0200

    Add files via upload

---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+const itemsEl = document.querySelectorAll("#categories, item");
+console.log(`Number of categories ${item.length} `);
+
+items = forEach (item => {
+    const categoryTitile = item.querSelector("h2").textContent ;
+    const elementsCount = item.querSelector("li").length ; 
+
+    console.log(`Categories: ${categoryTitile}`)
+    console.log(``)
+
+})
+
