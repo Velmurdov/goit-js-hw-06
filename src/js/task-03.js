052b2bc3467e5e89150262954369619ce396f5ce
Velmurdov <batuvalll@gmail.com>
2023-06-18 13:38:02 +0200

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+
+
+const makeGalleryItemEl = ({ url, alt }) => {
+  const galleryListEl = `<li class="item"><img class="img" src="${url}" alt="${alt}" width="350"></li>`;
+  galleryEl.insertAdjacentHTML('beforeend', galleryListEl);
+};
+
+const galleryEl = document.querySelector('.gallery');
+const galleryList = images.map(makeGalleryItemEl);
+
