f24a38f39c12beb22a9a15541e1b7e38d7353752
Velmurdov <batuvalll@gmail.com>
2023-06-20 18:44:38 +0200

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,10 @@
+
+const changeColorEl.document.querySelector('.change-color')
+
+changeColorEl.
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
